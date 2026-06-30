5a8b52eff160124b4aeff60ca40986ebe024c0b5
lipeng <lp02388651@alibaba-inc.com>
2026-06-30 17:56:02 +0800

    feat: rebulid project

--- src/main/java/com/bookorder/mapper/BorrowRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bookorder.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bookorder.entity.BorrowRecord;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BorrowRecordMapper extends BaseMapper<BorrowRecord> {
-}
--- src/main/java/com/bookorder/mapper/SysPermissionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bookorder.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bookorder.entity.SysPermission;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysPermissionMapper extends BaseMapper<SysPermission> {
-}
