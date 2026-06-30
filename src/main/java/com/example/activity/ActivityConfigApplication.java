8ed240a302d107160e6c1b43bb318a1e51b440d5
lipeng <lp02388651@alibaba-inc.com>
2026-06-30 17:56:11 +0800

    feat: rebulid project

---
+++ src/main/java/com/example/activity/ActivityConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityConfigApplication.class, args);
+    }
+
+}
